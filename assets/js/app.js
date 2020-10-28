bac7febfa5869c4ec3330323244083fb1a9e9dac
santangelo-v <vincenzo.santangelo@prima.it>
2020-10-28 17:45:40 +0100

    Add bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+// import "../css/app.scss"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -13,3 +14,5 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+import "bootstrap"
+import regeneratorRuntime from "regenerator-runtime";
