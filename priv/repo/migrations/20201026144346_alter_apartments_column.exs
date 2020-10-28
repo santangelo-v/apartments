c13d0f036d66022f3a0d9c9e6b1550d6c294254a
santangelo-v <vincenzo.santangelo@prima.it>
2020-10-28 11:23:59 +0100

    Remove calendar entity and table

--- priv/repo/migrations/20201026144346_alter_apartments_column.exs
+++
@@ -1,9 +0,0 @@
-defmodule Apartments.Repo.Migrations.AlterApartmentsColumn do
-  use Ecto.Migration
-
-  def change do
-    alter table(:apartments) do
-      modify :vacancies, :integer
-    end
-  end
-end
